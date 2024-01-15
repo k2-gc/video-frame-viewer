ad178f6b3bbc2bec2553e747d2ee876939138e2e
ka2u-gc <h.13soccer.k@gmail.com>
2024-01-16 00:12:20 +0900

    Add operations by keyboard pressing

--- src/frame_viewer/app_controller.py
+++ src/frame_viewer/app_controller.py
@@ -10,6 +10,11 @@ class Controller:
         self.model = Model(video_path=video_path)
         image_width, image_height = self.model.get_image_size()
         self.view = View(root, (image_width, image_height))
+        self.root = root
+        self.next_key = ["d", "Right"]
+        self.prev_key = ["a", "Left"]
+        self.save_key = ["s", "Down"]
+        self.quit_key = ["q"]
 
         # Bind variables in Model to widgets in View class
         self.view.current_frame.config(textvariable=self.model.currnet_frame_index)
@@ -19,6 +24,9 @@ class Controller:
         self.view.next_btn.config(command=lambda: self.update_frame(True))
         self.view.prev_btn.config(command=lambda: self.update_frame(False))
         self.view.save_btn.config(command=self.model.save_frame)
+
+        # Bind callback function to key pressing
+        self.root.bind("<KeyPress>", self.key_event)
 
         # Display first frame
         self._update_image()
@@ -30,3 +38,20 @@ class Controller:
     def _update_image(self):
         image = self.model.show_frame()
         self.view.update_image(image)
+
+    def key_event(self, event):
+        if event.keysym in self.next_key:
+            self.update_frame(is_next=True)
+            return
+        elif event.keysym in self.prev_key:
+            self.update_frame(is_next=False)
+            return
+        elif event.keysym in self.save_key:
+            self.model.save_frame()
+            return
+        elif event.keysym in self.quit_key:
+            self.logger.info("App quitting...")
+            self.root.destroy()
+            self.root.quit()
+        else:
+            self.logger.info(f"No functions is assigned to key '{event.keysym}'")
